acb0c0d1665b83a8c00271a8e75166c2d2c3c8d3
yuki00yossi <yuki00yoshioka@gmail.com>
2023-09-19 23:46:07 +0900

    JSONを返すように変更

--- account/views.py
+++ account/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+# from django.shortcuts import render
 
 from account.models import UserActivationToken
 
 
 # Create your views here.
 def activate_user(request, token):
+    """ユーザーをアクティベートするAPI
+
+    Args:
+        request (_type_):
+        token (str): アクティベーショントークン
+
+    Returns:
+        JsonResponse: JsonResponse。
+            成功時のステータスコードは200。失敗時は400
+    """
     result = UserActivationToken.objects.activate_user(token)
-    return render(
-        request,
-        'account/activate_user.html',
-        {'result': result, }
-    )
+    if result:
+        return JsonResponse({}, status=200)
+    return JsonResponse({}, status=400)
