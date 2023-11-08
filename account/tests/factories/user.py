b1fe5f67bba6a37df6f2e1da4bcbfc8019df312f
吉岡優樹 <yoshiokayuuki@yuki.local>
2023-11-08 23:16:13 +0900

    ユーザーファクトリーにパスワード追加

--- account/tests/factories/user.py
+++ account/tests/factories/user.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta
 
 from factory import Faker, PostGenerationMethodCall, Sequence
-from factory.django import DjangoModelFactory
+from factory.django import DjangoModelFactory, Password
 
 from django.contrib.auth import get_user_model
-
 
 Faker._DEFAULT_LOCALE = 'ja_JP'
 
@@ -18,6 +17,7 @@ class UserFactory(DjangoModelFactory):
     first_name = Faker('first_name')
     last_name = Faker('last_name')
     profile_text = Faker('text')
+    password = Password('password')
     is_staff = False
     is_active = True
     is_superuser = False
