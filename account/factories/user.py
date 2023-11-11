027e93379822a5296b5218f12edaf16dcce2cb22
yuki00yossi <yuki00yoshioka@gmail.com>
2023-11-11 17:04:25 +0900

    ユーザーシーダー追加

--- account/factories/user.py
+++ account/factories/user.py
@@ -1,3 +1,4 @@
+import factory
 from factory import Faker
 from factory.django import DjangoModelFactory, Password
 
@@ -19,3 +20,31 @@ class UserFactory(DjangoModelFactory):
     is_staff = False
     is_active = True
     is_superuser = False
+
+    @classmethod
+    def seed_user(cls, user_num=50, staff_num=10, super_num=5):
+        """ユーザーのダミーデータを挿入する
+
+        Args:
+            user_num (int, optional): 追加する通常ユーザー数 Defaults to 100.
+            staff_num (int, optional): 追加するスタッフ権限ユーザー数. Defaults to 10.
+            super_num (int, optional): 追加するスーパーユーザー数. Defaults to 5.
+        """
+        # 通常ユーザーを作成
+        cls.create_batch(
+            user_num,
+            email=factory.Sequence(lambda n: 'user{0}@example.com'.format(n)),
+        )
+        # スタッフユーザーを作成
+        cls.create_batch(
+            staff_num,
+            email=factory.Sequence(lambda n: 'staff{0}@example.com'.format(n)),
+            is_staff=True
+        )
+        # スーパーユーザーを作成
+        cls.create_batch(
+            super_num,
+            email=factory.Sequence(lambda n: 'super{0}@example.com'.format(n)),
+            is_staff=True,
+            is_superuser=True
+        )
