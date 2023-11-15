40fc0bc9a515241d9471708688d7caa13ae8f179
吉岡優樹 <yuki00yoshioka@gmail.com>
2023-11-15 23:58:46 +0900

    Flake8警告対応

--- account/serializer.py
+++ account/serializer.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-from organization.serializer import OrganizationSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
