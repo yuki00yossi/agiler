67903a33972b2b886213d65b1c58e0f7c4383bfa
yuki00yossi <yuki00yoshioka@gmail.com>
2023-12-03 18:21:25 +0900

    プロジェクトモデル編集

--- project/models.py
+++ project/models.py
@@ -7,4 +7,22 @@ from organization.models import Organization
 class Project(models.Model):
     """プロジェクトのモデル"""
     name = models.CharField(max_length=256, verbose_name='プロジェクト名')
-    detail = models.CharField(max_length=1000, verbose_name='詳細')
+    detail = models.CharField(max_length=1000, verbose_name='プロジェクト詳細')
+    organization = models.ForeignKey(
+        Organization,
+        verbose_name='組織',
+        on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name='作成日', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True)
+
+    REQUIRE_FIELDS = [
+        'name', 'detail', 'organization',
+    ]
+
+    class Meta:
+        db_table_comment='プロジェクトテーブル'
+        verbose_name = 'プロジェクト'
+        verbose_name_plural = 'プロジェクト'
+
+    def __str__(self):
+        return self.name
