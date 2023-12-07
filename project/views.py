e429596216c3d88764dfebcfcb03e71f2e173b36
yuki00yossi <yuki00yoshioka@gmail.com>
2023-12-07 23:53:57 +0900

    プロジェクトビューセット作成

--- project/views.py
+++ project/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from project.serializer import ProjectSerializer
+from project.models import Project
 
 # Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    """ プロジェクトのビュー """
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
